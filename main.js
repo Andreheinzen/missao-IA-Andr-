6ca099c3d34a2b364c0c5027d780958456fb4d5c
André <145787269+Andreheinzen@users.noreply.github.com>
2025-10-21 14:52:04 -0300

    sdsds

--- main.js
+++ main.js
@@ -75,10 +75,18 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
-
+let historiaFinal
 function mostraPergunta(){
-  perguntaAtual = perguntas[atual];
+ if(atual>= perguntas.length){
+   mostraResultado()
+   return;
+
+ }
+ 
+ 
+    perguntaAtual = perguntas[atual];
   caixaPerguntas.textContent = perguntaAtual.enunciado
+  caixaAlternativas.textContent = "  ";
   mostraAlternativas();
 }
 
@@ -86,13 +94,26 @@ function mostraAlternativas(){
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto
-        botaoAlternativas.addEventListener('click',function(){
-        atual++;
-        mostraPergunta():
-        })
+        botaoAlternativas.addEventListener('click',  () => respostaSelecionada(alternativa));
        caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
+
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal
+    atual++
+    mostraPergunta();
+
+}
+
+function mostraResultado(){
+    caixaPerguntas.textContent = "Se fosse possível...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "  ";
+}
+
 
 
 
